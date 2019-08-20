ce6b0ef7753b7f39f2972b5deaddca8da38ae60b
Dante Fragapane <dante.fragapane@gmail.com>
2019-08-20 19:45:13 -0400

    Added experience

--- game.js
+++ game.js
@@ -1,11 +1,1 @@
-const Ship = require('./public/classes/ship')
 const Chars = require('./public/classes/characters')
-
-const bionic = new Chars.Bionic('Dante', {
-  strength: 10,
-  constitution: 10,
-  dexterity: 10,
-  intelligence: 10,
-  widom: 10,
-  charisma: 10
-})
