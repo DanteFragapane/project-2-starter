0de0a2bc7977efd9b3dc033334b531b4828b4b37
Dante Fragapane <dante.fragapane@gmail.com>
2019-08-24 17:54:33 -0400

    Finished Alien

--- public/classes/alien.js
+++ public/classes/alien.js
@@ -13,7 +13,67 @@ class Alien extends Character.Character {
     }
   ) {
     super(name, stats)
+    this.descriptions = {
+      forceWave: 'Default skill. Activates boot thrusters to kick the crap out of enemies.',
+      telekeneticProjectiles:
+        'Default skill. Use the power of mind to manipulate physical matter and fire medium-sized objects at high speeds.',
+      cognitiveShock:
+        'Project beams of mental energy at enemy weak points in an attempt at extra damage. Doesn’t always work.',
+      psiScream:
+        'Unleash 3 pulses of psycho-keneticpower that attack enemy mental faculties. Scales with Intelligence.',
+      orbitalSingularity:
+        'Telekenetically generate black holes around your enemy and watch as he gets pulled apart molecule by molecule.',
+      psychicRegen: 'Elevate mind over matter and imagine the pain away for continued fighting.'
+    }
+  }
+
+  forceWave () {
+    return Character.getRandomInt(2, 4)
+  }
+
+  telekeneticProjectiles () {
+    if (this.energy > 0) {
+      this.energy--
+      return Character.getRandomInt(3, 4)
+    }
+    return 0
+  }
+
+  cognitiveShock () {
+    if (this.energy > 1) {
+      this.energy -= 2
+      return Character.getRandomInt(1, 7)
+    }
+    return 0
+  }
+
+  psiScream () {
+    if (this.energy > 2) {
+      this.energy -= 3
+      return (
+        Character.getRandomInt(1, 2) *
+        Character.getRandomInt(1, 2) *
+        Character.getRandomInt(1, 2) *
+        (this.intelligence / 4)
+      )
+    }
+    return 0
+  }
+
+  orbitalSingularity () {
+    if (this.energy > 5) {
+      this.energy -= 6
+      return 8 * (this.wisdom / 4)
+    }
+    return 0
+  }
+
+  psychicRegen () {
+    if (this.energy > 2) {
+      this.energy -= 3
+      return -5 * (this.utility / 2)
+    }
+    return 0
   }
 }
-
 module.exports = Alien
