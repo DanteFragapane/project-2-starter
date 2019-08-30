f3ccc137cd1e9991695c1518677cb37319b30bfb
RDimbo <r.dimbo@outlook.com>
2019-08-29 20:19:19 -0400

    trouble shoot this

---
+++ public/game.js
@@ -0,0 +1,188 @@
+const Alien = require('./public/classes/alien')
+const Bionic = require('./public/classes/cyborg')
+const Human = require('./public/classes/human')
+const Machine = require('./public/classes/machine')
+
+const dante = new Human('Dante')
+
+//dante.unlock('scopeShot')
+//console.log(dante.mervGrenade())
+// Combat engine
+$(document).ready(function (){
+    
+    function theFight() {
+
+    $("#skill-01").click(function () {
+        //console.log("skill-01");
+
+    });
+
+    $("#skill-02").click(function () {
+        console.log("skill-02");
+    });
+
+    $("#skill-03").click(function () {
+        console.log("skill-03");
+    });
+
+    $("#skill-04").click(function () {
+        console.log("skill-04");
+    });
+
+    $("#skill-05").click(function () {
+        console.log("skill-05");
+    });
+
+    $("#skill-06").click(function () {
+        console.log("skill-06");
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+theFight();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
