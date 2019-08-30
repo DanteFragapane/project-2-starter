dc4240ddbd9c92e5fe2d75d566eb2d690b40e0d0
Tony Romaniuk <tony6225@live.com>
2019-08-30 17:08:11 -0400

    Login handlebars work

--- routes/html.js
+++ routes/html.js
@@ -9,9 +9,6 @@ router
   .get('/signup', (_, res) => {
     res.render('signup')
   })
-  .get('/login', (_, res) => {
-    res.render('login')
-  })
   .get('/game', (_, res) => {
     res.render('game')
   })
