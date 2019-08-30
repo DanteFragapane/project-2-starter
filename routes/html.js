40677bc7dcfeb350d201c89d67c34a13590e1e17
Dante Fragapane <dante.fragapane@gmail.com>
2019-08-29 20:10:46 -0400

    Got rid of  setcookie route, and cookie argument

--- routes/html.js
+++ routes/html.js
@@ -2,10 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/setcookie', (_, res) => {})
-
 router.get('/', (_, res) => {
-  res.render('index', { cookie: _.cookies.cookieName })
+  res.render('index')
 })
 
 module.exports = router
