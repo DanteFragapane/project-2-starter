f5e1bfa810cbb1717eb8c009c2438429e36c3a65
Tony Romaniuk <tony6225@live.com>
2019-08-30 16:54:44 -0400

    Updated html.js with route for login.handlebars

--- routes/html.js
+++ routes/html.js
@@ -9,6 +9,9 @@ router
   .get('/signup', (_, res) => {
     res.render('signup')
   })
+  .get('/login', (_, res) => {
+    res.render('login')
+  })
   .get('/game', (_, res) => {
     res.render('game')
   })
