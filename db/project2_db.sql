e59cab15d102bbbc65d7a9a1ff46bcf56248bbb7
RDimbo <r.dimbo@outlook.com>
2019-09-03 19:47:12 -0400

    database updated

--- db/project2_db.sql
+++ db/project2_db.sql
@@ -1,16 +1,11 @@
 USE project2_db;
-CREATE TABLE users
-(
-  username VARCHAR
-  (50) PRIMARY KEY UNIQUE NOT NULL,
-  userpword TEXT NOT NULL,
-  charactername VARCHAR
-  (100) NOT NULL,
-  characterlevel INT
-  (5),
-  characterxp INT
-  (10),
-  charactertype VARCHAR
-  (25),
-  cookie TEXT
+CREATE TABLE users (
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(50) NOT NULL,
+userpword  VARCHAR(50) NOT NULL,
+charactername VARCHAR(100),
+characterlevel INT(5),
+characterxp INT(10),
+charactertype VARCHAR(25),
+cookie TEXT
 );
