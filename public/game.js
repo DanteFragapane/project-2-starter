9980e8cb0a783320cb3834c42b27d8ecf227ddf7
Dante Fragapane <dante.fragapane@gmail.com>
2019-09-05 10:46:06 -0400

    Fixed a bug, moved around functions

--- public/game.js
+++ public/game.js
@@ -1,8 +1,8 @@
 import Human from './human.js'
 import Machine from './machine.js'
 
-const player = new Human("Stalker")
-const enemy = new Machine("Fer-de-Lance")
+const player = new Human('Stalker')
+const enemy = new Machine('Fer-de-Lance')
 
 // If the logout button is clicked
 $('#logout').click(
@@ -14,8 +14,18 @@ $('#logout').click(
 
 // Combat engine
 $(document).ready(function () {
+  // Immediately update the stats on the page
   updateStats()
-  
+
+  function enemyDies () {
+    $('#battle-log-text-03').html('Congratulations!! You have vanquished your foe!!')
+  }
+
+  function playerLoses () {
+    $('#battle-log-text-03').html('You have been defeated!!')
+    updateStats()
+  }
+
   function commitStats () {
     $.ajax({
       url: '/api/commitStats',
@@ -25,10 +35,9 @@ $(document).ready(function () {
   }
 
   function updateStats () {
-
-  commitStats()
-  player.energy +=2
-  enemy.energy +=2
+    commitStats()
+    player.energy += 2
+    enemy.energy += 2
     $('#player-card-health').html(`Health: ${player.health}`)
     $('#player-card-energy').html(`Energy: ${player.energy}`)
     $('#enemy-card-health').html(`Health: ${enemy.health}`)
@@ -36,7 +45,7 @@ $(document).ready(function () {
     theFight()
   }
 
-  function theFight() {
+  function theFight () {
     $('#skill-01').click(function () {
       console.log('skill-01')
       const damage = player.rocketKick()
@@ -93,7 +102,6 @@ $(document).ready(function () {
           `You engaged the MIRV GRENADE against your enemy for ${player.mirvGrenade()} damage!`
         )
         enemy.health -= damage
-        
       } else {
         $('#battle-log-text').html("You don't have enough energy for that!")
       }
@@ -105,9 +113,7 @@ $(document).ready(function () {
       console.log('skill-05')
       const damage = player.orbitalStrike()
       if (damage !== 0) {
-        $('#battle-log-text').html(
-         ` You engaged the ORBITAL STRIKE against your enemy for ` + damage +` damage!`
-        )
+        $('#battle-log-text').html(` You engaged the ORBITAL STRIKE against your enemy for ` + damage + ` damage!`)
         enemy.health -= damage
         if (enemy.health <= 0) {
           enemyDies()
@@ -124,7 +130,8 @@ $(document).ready(function () {
       const heal = player.stimInjection()
       if (heal !== 0) {
         $('#battle-log-text').html(
-          `You engaged the STIM INJECTION to recover some health for` + heal + `points reovered!`)
+          `You engaged the STIM INJECTION to recover some health for` + heal + `points reovered!`
+        )
         player.health += heal
       } else {
         $('#battle-log-text').html("You don't have enough energy for that!")
@@ -132,75 +139,56 @@ $(document).ready(function () {
       enemyMove()
       updateStats()
     })
-  
 
-  function enemyMove() {
-    // If enemy health is half or less => prioritize healing!
-    if (enemy.health <= enemy.maxHealth / 2 && enemy.energy >= 3) {
-      const addHealth = enemy.repairBots();
-      enemy.health += addHealth;
-      console.log("+health " + addHealth)
-      $('#battle-log-text-02').html(`Your enemy healed for `+ addHealth + ` points health!`)
-    } else
-
-      // If enemy.energy >=6 then deathbringer
-      if (enemy.energy >= 6) {
+    function enemyMove () {
+      // If enemy health is half or less => prioritize healing!
+      if (enemy.health <= enemy.maxHealth / 2 && enemy.energy >= 3) {
+        const addHealth = enemy.repairBots()
+        enemy.health += addHealth
+        console.log('+health ' + addHealth)
+        $('#battle-log-text-02').html(`Your enemy healed for ` + addHealth + ` points health!`)
+      } else if (enemy.energy >= 6) {
+        // If enemy.energy >=6 then deathbringer
         const damage = enemy.deathbringer()
         $('#battle-log-text-02').html(`Your Enemy slashed your for ` + damage + ` of damage`)
-        console.log("death " + damage)
+        console.log('death ' + damage)
         player.health -= damage
-        if(player.health <=0){
+        if (player.health <= 0) {
           playerLoses()
         }
-      }
-    else
-      // If enemy.energy >=3 then pistonjab
-      if (enemy.energy >= 3) {
+      } else if (enemy.energy >= 3) {
+        // If enemy.energy >=3 then pistonjab
         const damage = enemy.pistonJab()
 
         $('#battle-log-text-02').html(`Your Enemy pummelled you for ` + damage + ` of damage`)
         player.health -= damage
-        console.log("piston " + damage)
-        if(player.health <=0){
+        console.log('piston ' + damage)
+        if (player.health <= 0) {
           playerLoses()
         }
-      }
-    else
-      // If enemy.energy >= 2 then railgun
-      if (enemy.energy >= 2) {
+      } else if (enemy.energy >= 2) {
+        // If enemy.energy >= 2 then railgun
         const damage = enemy.railGun()
         $('#battle-log-text-02').html(`Your Enemy blasted you for ` + damage + ` of damage`)
-        console.log("RG " + damage)
+        console.log('RG ' + damage)
         player.health -= damage
-        if(player.health <=0){
+        if (player.health <= 0) {
           playerLoses()
         }
-      }
-      else if(enemy.energy >=1){
+      } else if (enemy.energy >= 1) {
         const damage = enemy.grappleArmDropkick()
-        $('#battle-log-text-02').html(`Your enemy kicked you for ` + damage  +` points of damage!`)
+        $('#battle-log-text-02').html(`Your enemy kicked you for ` + damage + ` points of damage!`)
         player.health -= damage
-        if(player.health <=0){
+        if (player.health <= 0) {
           playerLoses()
-        }else{
-        const damage = heavyMetalPileDriver()
-        player.health -= damage
-        if(player.health <=0){
-          playerLoses()
+        } else {
+          const damage = enemy.heavyMetalPileDriver()
+          player.health -= damage
+          if (player.health <= 0) {
+            playerLoses()
+          }
         }
       }
-      
+    }
   }
-
-
-
-  function enemyDies() {
-    $('#battle-log-text-03').html("Congratulations!! You have vanquished your foe!!")
-   }
-
-   function playerLoses(){
-     $('#battle-log-text-03').html("You have been defeated!!")
-     updateStats()
-   }
-  
-}}})
+})
